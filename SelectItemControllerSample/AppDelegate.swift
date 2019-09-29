7a61318ed07c9c671d4ec4f5d766f21ed0a0d3af
keygx <y.kagiyama@gmail.com>
2019-09-29 19:21:26 +0900

    Support to Dark Mode in Base Project

--- SelectItemControllerSample/AppDelegate.swift
+++ SelectItemControllerSample/AppDelegate.swift
@@ -2,45 +2,49 @@
 //  AppDelegate.swift
 //  SelectItemControllerSample
 //
-//  Created by keygx on 2017/03/14.
-//  Copyright © 2017年 keygx. All rights reserved.
+//  Created by keygx on 2019/09/29.
+//  Copyright © 2019 keygx. All rights reserved.
 //
 
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
-
+    lazy var appEventHandler = AppEventHandler()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if #available(iOS 13, *) {} else {
+            window = appEventHandler.createWindow()
+            window?.makeKeyAndVisible()
+        }
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
+    
+    func applicationWillTerminate(_ application: UIApplication) {}
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        appEventHandler.didBecomeActive()
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        appEventHandler.willResignActive()
     }
-
-
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        appEventHandler.willEnterForeground()
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        appEventHandler.didEnterBackground()
+    }
+    
+    // MARK: UISceneSession Lifecycle
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
 }
-
